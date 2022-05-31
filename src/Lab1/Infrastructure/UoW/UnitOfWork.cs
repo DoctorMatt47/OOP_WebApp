1761b7b7cf85a513f38e6cc9ff499de50d8d0b15
m.holtsev <DoctorMatt47@gmail.com>
2022-06-01 02:14:39 +0300

    Added unit of work implementation

---
+++ src/Lab1/Infrastructure/UoW/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Common;
+using Application.Common.Interfaces;
+using Infrastructure.Repositories;
+using Npgsql;
+
+namespace Infrastructure.UoW;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbConnection _connection;
+    private readonly DbTransaction _transaction;
+
+    public UnitOfWork(string connectionString)
+    {
+        _connection = new NpgsqlConnection(connectionString);
+        _connection.Open();
+        _transaction = _connection.BeginTransaction();
+
+        Options = new OptionRepository(_connection);
+        Questions = new QuestionRepository(_connection);
+        Tests = new TestRepository(_connection);
+    }
+
+    public IOptionRepository Options { get; }
+    public IQuestionRepository Questions { get; }
+    public ITestRepository Tests { get; }
+
+    public Task SaveChangesAsync() => _transaction.CommitAsync();
+
+    public async ValueTask DisposeAsync()
+    {
+        await _transaction.DisposeAsync();
+        await _connection.CloseAsync();
+        await _connection.DisposeAsync();
+    }
+}
---
+++ src/Lab1/Infrastructure/UoW/UnitOfWorkFactory.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.UoW;
+
+public class UnitOfWorkFactory : IUnitOfWorkFactory
+{
+    private readonly string _connectionString;
+
+    public UnitOfWorkFactory(IConfiguration configuration) =>
+        _connectionString = configuration
+            .GetSection("ConnectionStrings")
+            .GetSection("OOP_WebApp").Value;
+
+    public IUnitOfWork Create() => new UnitOfWork(_connectionString);
+}
