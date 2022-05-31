89be120dfdbc5bd01b26f256ebd6969650b08f39
m.holtsev <DoctorMatt47@gmail.com>
2022-06-01 01:03:58 +0300

    Added uow interface

---
+++ src/Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IOptionRepository Options { get; }
+    IQuestionRepository Questions { get; }
+    ITestRepository Tests { get; }
+}
