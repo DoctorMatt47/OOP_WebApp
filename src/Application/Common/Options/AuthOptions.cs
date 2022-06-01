56b4b36e88a37dc8693b23358b2bf4fdb52c2fc8
m.holtsev <DoctorMatt47@gmail.com>
2022-06-01 21:37:30 +0300

    Added auth options

---
+++ src/Application/Common/Options/AuthOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Common.Options;
+
+public static class AuthOptions
+{
+    public const string Issuer = "OOP_WebApp-AuthServer";
+    public const string Audience = "OOP_WebApp-AuthClient";
+    private const string Key = "OOP_WebApp-SecretKey";
+
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() => new(Encoding.ASCII.GetBytes(Key));
+}
