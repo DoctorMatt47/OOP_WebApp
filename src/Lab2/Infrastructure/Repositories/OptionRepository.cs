725217e8c70850b6dbdf925e8e2aa2291a7c06d6
m.holtsev <DoctorMatt47@gmail.com>
2022-06-01 22:07:23 +0300

    Added option repository lab 2

---
+++ src/Lab2/Infrastructure/Repositories/OptionRepository.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using Lab2.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using OOP_WebApp.Domain.Entities;
+
+namespace Lab2.Infrastructure.Repositories;
+
+public class OptionRepository : RepositoryBase, IOptionRepository
+{
+    public OptionRepository(OopWebAppContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Option>> Get(QuestionId id, CancellationToken cancellationToken) =>
+        await Context.Options
+            .Where(o => o.QuestionId == id)
+            .ToListAsync(cancellationToken);
+
+    public Task Create(IEnumerable<Option> options, CancellationToken cancellationToken)
+    {
+        Context.Options.AddRange(options);
+        return Task.CompletedTask;
+    }
+}
