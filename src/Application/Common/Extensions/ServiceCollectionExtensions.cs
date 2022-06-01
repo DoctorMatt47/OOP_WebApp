b8f944f5265bfad4628b48cf79609aae02c91ece
m.holtsev <DoctorMatt47@gmail.com>
2022-06-01 21:38:18 +0300

    Added service extensions

---
+++ src/Application/Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Identity;
+using Application.Tests;
+using Application.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Common.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services) =>
+        services
+            .AddTransient<IJwtTokenService, JwtTokenService>()
+            .AddTransient<IHashService, HashService>()
+            .AddTransient<ITestService, TestService>()
+            .AddTransient<IUserService, UserService>();
+}
---
+++ src/Lab1/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.UoW;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services) =>
+        services.AddScoped<IUnitOfWorkFactory, UnitOfWorkFactory>();
+}
