0291e4646b67d052984b810bb962b96c669cc5b0
m.holtsev <DoctorMatt47@gmail.com>
2022-06-05 12:24:27 +0300

    Added answers controller

---
+++ src/Lab2/WebAPI/Controllers/AnswersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OOP_WebApp.Application.Answers;
+using OOP_WebApp.Domain.Entities;
+
+namespace OOP_WebApp.Lab2.WebAPI.Controllers;
+
+public class AnswersController : ApiControllerBase
+{
+    private readonly IAnswerService _answers;
+
+    public AnswersController(IAnswerService answers) => _answers = answers;
+
+    [HttpGet]
+    [Authorize(Roles = "Tutor")]
+    public Task<IEnumerable<GetAnswerResponse>> Get(
+        [FromQuery] Guid testId,
+        [FromQuery] string username,
+        CancellationToken cancellationToken) =>
+        _answers.Get(TestId.From(testId), Username.From(username), cancellationToken);
+}
